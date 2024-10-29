2a33c96d9e0716d424438d98f0a2f8928461920d
Diogo Barbosa <diogo.barbosa@inov.pt>
2024-10-29 12:40:54 +0000

    Harpoon kEyMaPs

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,1 +1,12 @@
-return { 'ThePrimeagen/harpoon' }
+return {
+  'ThePrimeagen/harpoon',
+  keys = {
+    { 'hx', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
+    { 'ha', '<cmd>lua require("harpoon.mark").add_file()<cr>' },
+    { 'hx', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
+    { 'hh', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
+    { 'hj', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
+    { 'hk', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
+    { 'hl', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
+  },
+}
