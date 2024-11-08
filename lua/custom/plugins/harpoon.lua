bb2bd7d0869e35b8b4b3a9a75cc93ce69e6a398d
Diogo Barbosa <diogo.barbosa@inov.pt>
2024-11-08 10:37:45 +0000

    harpoon config

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,12 +1,12 @@
 return {
   'ThePrimeagen/harpoon',
   keys = {
-    { 'hx', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
-    { 'ha', '<cmd>lua require("harpoon.mark").add_file()<cr>' },
-    { 'hx', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
-    { 'hh', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
-    { 'hj', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
-    { 'hk', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
-    { 'hl', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
+    { '<leader>x', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
+    { '<leader>a', '<cmd>lua require("harpoon.mark").add_file()<cr>' },
+    { '<leader>x', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
+    { '<leader>h', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
+    { '<leader>j', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
+    { '<leader>k', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
+    { '<leader>l', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
   },
 }
